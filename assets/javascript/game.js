d3c4b4b6d09b8d0eb9d8e045668449a2424a9f96
Paul Ballas <paul.ballas@richmond.edu>
2019-11-08 18:24:50 -0500

    make progress on HTML and CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,17 @@
 
 // initializing variables
 let guess;
-let guesses = [];
+var guessArray = [];
 var wins;
+var losses;
 var lives;
 var correct = 0;
 var valid;
+var containerElement;
+var computerInput;
+var rand;
 var createSpan;
+var currentlyPlaying;
 var alphabet = [
   "a",
   "b",
@@ -36,6 +41,7 @@ var alphabet = [
   "y",
   "z"
 ];
+
 // array of possible hangman words
 var countries = [
   "armenia",
@@ -56,38 +62,47 @@ var countries = [
   "uzbekistan"
 ];
 
+var computerSelection = computerPick();
+console.log(computerSelection);
+
 // Start game at pageload function
-window.addEventListener("load", event => {
-  console.log("the page has loaded");
+function startRound() {
   board();
+  computerPick();
+  // keydownFunction();
   wins = 0;
   lives = 10;
+  guessArray.length = 0;
+  document.addEventListener("keydown", keydownFunction);
+}
+
+window.addEventListener("load", event => {
+  console.log("the page has loaded");
+  startRound();
 });
 
-var guessArray = [];
 // HTML elements
 var livesElement = document.getElementById("lives");
 var wordElement = document.getElementById("Word");
+var winsElement = document.getElementById("wins");
+var lossesElement = document.getElementById("losses");
 var gameOverElement = document.querySelector(".gameOver");
 var allGuess = document.getElementById("guess");
 var letter;
 // var wordCreate = document.createElement("div");
-var containerElement = document.getElementById("wordContainer");
-// assigns random number
-var rand = Math.floor(Math.random() * countries.length);
-// creates random computer input and splits into array
-var computerInput = countries[rand].split("");
-console.log(computerInput);
 
-// Creates answer Array
-let answerArray = [];
-for (var i = 0; i < computerInput.length; i++) {
-  answerArray[i] = "_";
+function computerPick() {
+  containerElement = document.getElementById("wordContainer");
+  // assigns random number
+  rand = Math.floor(Math.random() * countries.length);
+  // creates random computer input and splits into array
+  computerInput = countries[rand].split("");
+  return computerInput;
 }
 
 // creates spans for each letter
 function board() {
-  for (var i = 0; i < computerInput.length; i++) {
+  for (var i = 0; i < computerSelection.length; i++) {
     letter = document.createElement("span");
     letter.id = "s" + i;
     letter.innerHTML = "_";
@@ -103,15 +118,15 @@ var keydownFunction = function(event) {
     wrongGuess(guess);
     gameOver();
   } else {
-    console.log("badgu");
+    console.log("what?");
   }
 };
 document.addEventListener("keydown", keydownFunction);
 
 // for loop to iterate through arrays
 function iterateGuess(guess) {
-  for (var i = 0; i < computerInput.length; i++) {
-    if (guess === computerInput[i]) {
+  for (var i = 0; i < computerSelection.length; i++) {
+    if (guess === computerSelection[i]) {
       correct++;
       guessRight(i);
     }
@@ -120,7 +135,7 @@ function iterateGuess(guess) {
 
 // wrong guess function
 function wrongGuess(guess) {
-  if (computerInput.includes(guess) === false) {
+  if (computerSelection.includes(guess) === false) {
     lives--;
     livesElement.innerHTML = lives;
     guessArray.push(guess);
@@ -131,10 +146,27 @@ function wrongGuess(guess) {
 // when the player guesses right
 function guessRight(x) {
   var guessPosition = x;
-  guessArray.push(computerInput[x]);
+  guessArray.push(computerSelection[x]);
   allGuess.innerHTML = guessArray;
   var rightGuess = document.getElementById("s" + guessPosition);
-  rightGuess.innerHTML = computerInput[x];
+  rightGuess.innerHTML = computerSelection[x];
+  win(x);
+}
+
+// Wins function
+
+function win() {
+  if (correct === computerSelection.length) {
+    wins++;
+    winsElement.innerHTML = wins;
+  }
+}
+
+function loss() {
+  if (lives <= 0) {
+    losses++;
+    lossesElement.innerHTML = losses;
+  }
 }
 
 // gameover function to reset
@@ -143,15 +175,20 @@ function gameOver() {
     gameOverElement.innerHTML = "Game Over Comrade";
     document.removeEventListener("keydown", keydownFunction);
   }
+  currentlyPlaying = false;
 }
 
 // Reset Function
 var reset = document.addEventListener("click", function(event) {
-  console.log(event);
-  for (var i = 0; i < computerInput.length; i++) {
+  startRound();
+  for (var i = 0; i < computerSelection.length; i++) {
     var remove = document.getElementById("s" + i);
     wordElement.removeChild(remove);
   }
-  lives = 10;
-  board();
+  livesElement.innerHTML = 10;
+  guesses = [];
+  if (currentlyPlaying === false) {
+    gameOverElement.innerHTML = "";
+    allGuess.innerHTML = "";
+  }
 });
